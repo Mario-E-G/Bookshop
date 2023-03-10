7f03c3604f8579f05d09074f869cfd2c542a493c
Mario-E-G <marioehab330@gmail.com>
2023-03-10 17:03:22 +0200

    intializing some components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,27 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AboutComponent } from "./components/about/about.component";
+import { AuthorsComponent } from "./components/authors/authors.component";
+import { BooksComponent } from "./components/books/books.component";
+import { CategoriesComponent } from "./components/categories/categories.component";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { NotfoundComponent } from "./components/notfound/notfound.component";
+import { RegisterComponent } from "./components/register/register.component";
 
 const routes: Routes = [
- 
-  {path: "", component:RegisterComponent},
-  {path: "register", component:RegisterComponent},
-  {path: "login", component:LoginComponent}
+  { path: "", component: HomeComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "login", component: LoginComponent },
+  { path: "books", component: BooksComponent },
+  { path: "authors", component: AuthorsComponent },
+  { path: "categories", component: CategoriesComponent },
+  { path: "aboutus", component: AboutComponent },
+  { path: "**", component: NotfoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,27 @@ import { LoginComponent } from './components/login/login.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RegisterComponent } from './components/register/register.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { AboutComponent } from './components/about/about.component';
+import { BooksComponent } from './components/books/books.component';
+import { AuthorsComponent } from './components/authors/authors.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    NavbarComponent
+    NavbarComponent,
+    NotfoundComponent,
+    AboutComponent,
+    BooksComponent,
+    AuthorsComponent,
+    CategoriesComponent,
+    FooterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,9 +35,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
